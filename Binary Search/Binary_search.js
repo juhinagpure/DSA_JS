ae093cdc9a9c20c315a171d6fe2afb93a910e64e
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-02-25 23:10:27 +0530

    Binary Search with Iterative Approach

---
+++ Binary Search/Binary_search.js
@@ -0,0 +1,19 @@
+let data = [5, 9, 13, 17, 45, 67, 89, 100];
+let find = 17;
+let start = 0;
+let end = data.length - 1;
+let position = undefined;
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
+  //   console.warn(data[mid]);
+  //   console.warn(mid);
+  if (data[mid] === find) {
+    position = mid;
+    break;
+  } else if (data[mid] < find) {
+    start = mid + 1;
+  } else {
+    end = mid - 1;
+  }
+}
+console.warn(position);
